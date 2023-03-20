db77747bd436726f9627a6b6111cae87380d1f66
julia-ms <mombachdasilva@gmail.com>
2023-03-20 16:20:04 -0300

    separando significado #1

--- LimparDicionario/criandoCSV.py
+++ LimparDicionario/criandoCSV.py
@@ -4,6 +4,18 @@ import pathlib
 f = open('teste.csv', 'w', newline='', encoding='utf-8')
 strVar =''
 teste = 0
+
+
+listaClasses = ['abrev.','adj.','adv.','advers.','art.','auxil.','card.','caus.',
+                'comp.','compar.','conj.','contr.','coord.','cop.','def.', 'dem.','det.','disj.',
+                'elem.','f.','fin.','frac.','g.','indef.','integr.','interj.','interr.',
+                'intr.','loc.','m.','mult.','num.','núm.','ord.','pess.','pl.','poss.','pref.',
+                'prep.','pron.','rel.','s.','símb.','suf.','tr.','v.']
+
+
+#if 'elem.' in listaClasses:
+#    print("ta aquii")
+
 with open("texto.txt", "r", encoding="utf8") as x:
     for lines in x:
         if lines != "\n": 
@@ -11,26 +23,29 @@ with open("texto.txt", "r", encoding="utf8") as x:
             teste = 1
         else:
             if teste == 1:
-                #print(strVar + '\n')
-                #list(strVar)
+
                 tamanho = len(strVar)
                 varList = list(strVar)
                 for i in range (tamanho):
                     if varList[i] == ' ':
-                        varList[i] = ','
+                        varList[i] = ';'
+                        varI = i
                         break
-                #print(frog)
+                for i in range (varI,tamanho):
+                    if varList[i].isupper():
+                        varList[i-1] = ';'
+                        break
+                    if varList[i] == '1':
+                        varList[i-1] = ';'
+                        break
+                    if varList[i] == '[':
+                        varList[i-1] = ';'
+                        break
+                    
+                
                 linhaPronta =  ''.join(varList)
                 print(linhaPronta)
-                #print(frog[4:7])
-                #frog[4:5] = 'a'
-                #print(frog[4:5])
                 f.write(linhaPronta + '\n')
                 strVar = ''
                 teste = 0
-    # while lines != :
-    #     lines = x.readline()
-    #     print (lines)
-        
-    #     if lines == "\n":
-    #         lines = x.readline()
+   
