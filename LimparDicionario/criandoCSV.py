0c084d6357bc7c43ed7f911ef51d6f6c73fa1ff5
julia-ms <mombachdasilva@gmail.com>
2023-03-20 14:56:54 -0300

    Update criandoCSV.py

--- LimparDicionario/criandoCSV.py
+++ LimparDicionario/criandoCSV.py
@@ -1,20 +1,31 @@
 import csv
 import pathlib
 
-f = open('PalavraPorLinha.csv', 'w', newline='', encoding='utf-8')
+f = open('teste.csv', 'w', newline='', encoding='utf-8')
 strVar =''
 teste = 0
-with open("dicionario.txt", "r", encoding="utf8") as x:
+with open("texto.txt", "r", encoding="utf8") as x:
     for lines in x:
         if lines != "\n": 
             strVar = strVar+ lines[0:-2]
             teste = 1
         else:
             if teste == 1:
-                #if strVar == ' ':
-                    
-                print(strVar + '\n')
-                f.write(strVar + '\n')
+                #print(strVar + '\n')
+                #list(strVar)
+                tamanho = len(strVar)
+                varList = list(strVar)
+                for i in range (tamanho):
+                    if varList[i] == ' ':
+                        varList[i] = ','
+                        break
+                #print(frog)
+                linhaPronta =  ''.join(varList)
+                print(linhaPronta)
+                #print(frog[4:7])
+                #frog[4:5] = 'a'
+                #print(frog[4:5])
+                f.write(linhaPronta + '\n')
                 strVar = ''
                 teste = 0
     # while lines != :
