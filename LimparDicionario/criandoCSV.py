f27dc2ec4d75c6440131bb724c4072b05c824c96
julia-ms <mombachdasilva@gmail.com>
2023-03-20 15:02:38 -0300

    Palavra separada por virgula #1

--- LimparDicionario/criandoCSV.py
+++ LimparDicionario/criandoCSV.py
@@ -11,15 +11,22 @@ with open("dicionario.txt", "r", encoding="utf8") as x:
             teste = 1
         else:
             if teste == 1:
+<<<<<<< Updated upstream
                 #if strVar == ' ':
                     
                 print(strVar + '\n')
                 f.write(strVar + '\n')
+=======
+                tamanho = len(strVar)
+                varList = list(strVar)
+                for i in range (tamanho):
+                    if varList[i] == ' ':
+                        varList[i] = ','
+                        break
+                linhaPronta =  ''.join(varList)
+                print(linhaPronta)
+                f.write(linhaPronta + '\n')
+>>>>>>> Stashed changes
                 strVar = ''
                 teste = 0
-    # while lines != :
-    #     lines = x.readline()
-    #     print (lines)
-        
-    #     if lines == "\n":
-    #         lines = x.readline()
+
