80d1822bcbd0cab2634059b0f7073f93a6935359
ju-vargas <ju.rodrigues0100@gmail.com>
2023-04-13 07:03:27 -0300

    new implementation

--- horoscopo/src/TRIE.cpp
+++ horoscopo/src/TRIE.cpp
@@ -1,170 +1,175 @@
-#include <iostream>
-#include <fstream>
-#include <cstring>
-#include <string>
-#include <cstdlib>
-#include <ctime>
+#include "../include/bib.h"
 
 using namespace std;
-
-const int ALPHABET_SIZE = 26;
-
-class Trie {
-private:
-    struct Node {
-        bool isEndOfWord;
-        Node *children[ALPHABET_SIZE];
-
-        Node() {
-            isEndOfWord = false;
-            memset(children, 0, sizeof(children));
+ 
+// Define o tamanho do caractere
+#define CHAR_SIZE 128
+ 
+// Uma classe para armazenar um nó Trie
+class Trie
+{
+public:
+    bool isLeaf;
+    Trie* character[CHAR_SIZE];
+    int pos;
+ 
+    // Construtor
+    Trie()
+    {
+        this->isLeaf = false;
+ 
+        for (int i = 0; i < CHAR_SIZE; i++) {
+            this->character[i] = nullptr;
         }
 
-        ~Node() {
-            for (int i = 0; i < ALPHABET_SIZE; i++) {
-                delete children[i];
+        pos  = -1; 
+    }
+ 
+    void insert(string, streampos);
+    bool deletion(Trie*&, string);
+    Trie* search(string);
+    bool haveChildren(Trie const*);
+};
+ 
+// Função iterativa para inserir uma chave em um Trie
+void Trie::insert(string key, streampos pos)
+{
+    // inicia do nó raiz
+    Trie* curr = this;
+    for (int i = 0; i < key.length(); i++)
+    {
+        // cria um novo nó se o caminho não existir
+        if (curr->character[key[i]] == nullptr) {
+            curr->character[key[i]] = new Trie();
+        }
+ 
+        // vai para o próximo nó
+        curr = curr->character[key[i]];
+    }
+ 
+    // marca o nodo atual como uma folha
+    curr->isLeaf = true;
+    curr->pos = pos; 
+}
+ 
+// Função iterativa para pesquisar uma chave em um Trie. Ele retorna verdadeiro
+// se a chave for encontrada no Trie; caso contrário, retorna falso
+Trie* Trie::search(string key)
+{
+    // retorna false se Trie estiver vazio
+    if (this == nullptr) {
+        return nullptr;
+    }
+ 
+    Trie* curr = this;
+    for (int i = 0; i < key.length(); i++)
+    {
+        // vai para o próximo nó
+        curr = curr->character[key[i]];
+ 
+        // se a string for inválida (alcançou o final de um caminho no Trie)
+        if (curr == nullptr) {
+            return nullptr;
+        }
+    }
+ 
+    // retorna true se o nó atual for uma folha e o
+    // fim da string é atingido
+    return curr;
+}
+ 
+// Retorna true se um determinado nó tiver algum filho
+bool Trie::haveChildren(Trie const* curr)
+{
+    for (int i = 0; i < CHAR_SIZE; i++)
+    {
+        if (curr->character[i]) {
+            return true;    // filho encontrado
+        }
+    }
+ 
+    return false;
+}
+ 
+// Função recursivo para deletar uma chave no Trie
+bool Trie::deletion(Trie*& curr, string key)
+{
+    // retorna se Trie estiver vazio
+    if (curr == nullptr) {
+        return false;
+    }
+ 
+    // se o final da chave não for alcançado
+    if (key.length())
+    {
+        //recorre para o nó correspondente ao próximo caractere na chave
+        // e se retornar verdadeiro, exclui o nó atual (se não for folha)
+ 
+        if (curr != nullptr &&
+            curr->character[key[0]] != nullptr &&
+            deletion(curr->character[key[0]], key.substr(1)) &&
+            curr->isLeaf == false)
+        {
+            if (!haveChildren(curr))
+            {
+                delete curr;
+                curr = nullptr;
+                return true;
+            }
+            else {
+                return false;
             }
         }
-    };
-
-    Node *root;
-
-    void insert(Node *node, const string &key, int index) {
-        if (index == key.size()) {
-            node->isEndOfWord = true;
-            return;
-        }
-
-        int charIndex = key[index] - 'a';
-        if (!node->children[charIndex]) {
-            node->children[charIndex] = new Node();
-        }
-
-        insert(node->children[charIndex], key, index + 1);
     }
-
-    bool search(Node *node, const string &key, int index) const {
-        if (index == key.size()) {
-            return node->isEndOfWord;
+ 
+    // se o fim da chave for atingido
+    if (key.length() == 0 && curr->isLeaf)
+    {
+        // se o nodo atual for um nodo folha e não tiver filhos
+        if (!haveChildren(curr))
+        {
+            //deleta o nó atual
+            delete curr;
+            curr = nullptr;
+ 
+            // exclui os nós pais não-folha
+            return true;
         }
-
-        int charIndex = key[index] - 'a';
-        if (!node->children[charIndex]) {
+ 
+        // se o nodo atual for um nodo folha e tiver filhos
+        else {
+            // marca o nó atual como um nó não folha (DON'T DELETE IT)
+            curr->isLeaf = false;
+ 
+            // não exclui seus nós pais
             return false;
         }
-
-        return search(node->children[charIndex], key, index + 1);
     }
-
-public:
-    Trie() {
-        root = new Node();
-    }
-
-    ~Trie() {
-        delete root;
-    }
-
-    void insert(const string &key) {
-        insert(root, key, 0);
-    }
-
-    bool search(const string &key) const {
-        return search(root, key, 0);
-    }
-
-    void saveToFile(const char *fileName) const {
-        ofstream file(fileName, ios::binary);
-        saveToFile(root, file);
-    }
-
-    void loadFromFile(const char *fileName) {
-        ifstream file(fileName, ios::binary);
-        loadFromFile(root, file);
-    }
-
-    void clear() {
-        if (root) {
-            delete root;
-            root = nullptr;
-        }
-
-    }
-
-private:
-    void saveToFile(const Node *node, ofstream &file) const {
-        file.write((char*)&node->isEndOfWord, sizeof(node->isEndOfWord));
-        for (int i = 0; i < ALPHABET_SIZE; i++) {
-            if (node->children[i]) {
-                file.write((char*)&i, sizeof(i));
-                saveToFile(node->children[i], file);
-            }
-        }
-        int nullChar = -1;
-        file.write((char*)&nullChar, sizeof(nullChar));
-    }
-
-    void loadFromFile(Node *&node, ifstream &file) {
-        node = new Node();
-        file.read((char*)&node->isEndOfWord, sizeof(node->isEndOfWord));
-        int charIndex;
-        file.read((char*)&charIndex, sizeof(charIndex));
-        while (charIndex != -1) {
-            loadFromFile(node->children[charIndex], file);
-            file.read((char*)&charIndex, sizeof(charIndex));
-        }
-    }
-};
-
-int main() {
-    // Create a new Trie
-    Trie trie;
-
-    // Insert some words
-    trie.insert("apple");
-    trie.insert("banana");
-    trie.insert("cherry");
-    trie.insert("dog");
-    trie.insert("cat");
-    trie.insert("sk");
- /*   // Insert 100 random words
-    std::srand(std::time(nullptr)); // Seed the random number generator with the current time
-    const std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    for (int i = 0; i < 100000; i++) {
-        std::string word;
-        int length = std::rand() % 10 + 1; // Random word length between 1 and 10
-        for (int j = 0; j < length; j++) {
-            int index = std::rand() % alphabet.size(); // Random index in the alphabet string
-            word += alphabet[index]; // Add a random letter to the word
-        }
-        trie.insert(word);
-        if(i == 20000){
-            //cout << word << endl;
-            cout << "oi" << endl;
-        }
-    }
-*/
-
-    // Save the trie to a binary file
-    trie.saveToFile("trie.bin");
-
-    // Clear the trie to make sure we can load it back in correctly
-    trie.clear();
-
-    // Load the trie from the binary file
-    Trie loadedTrie;
-    loadedTrie.loadFromFile("trie.bin");
-
-    // Search for some words in the loaded trie
-    std::cout << loadedTrie.search("apple") << std::endl;  // true
-    std::cout << loadedTrie.search("banana") << std::endl; // true
-    std::cout << loadedTrie.search("cherry") << std::endl; // true
-    std::cout << loadedTrie.search("dog") << std::endl;    // true
-    std::cout << loadedTrie.search("cat") << std::endl;    // true
-    std::cout << loadedTrie.search("sk") << std::endl;    // true
-    std::cout << loadedTrie.search("bultdvmzkd") << std::endl;    // true
-    std::cout << loadedTrie.search("zebra") << std::endl;  // false
-
+ 
+    return false;
+}
+ 
+// Implementação C++ da estrutura de dados Trie
+int main()
+{
+    Trie* head = new Trie();
+ 
+    head->insert("hello",2);
+    cout << head->search("hello")->pos << " ";      // imprime 1
+ 
+    head->insert("helloworld",4);
+    cout << head->search("helloworld")->pos << " "; // imprime 1
+ 
+    //cout << head->search("helll")->ID << " ";      // imprime 0 (Não encontrado)
+ 
+    head->insert("hell",5);
+    cout << head->search("hell")->pos << " ";       // imprime 1
+ 
+    head->insert("h",6);
+    cout << head->search("h")->pos;                 // imprime 1
+ 
+    cout << endl;
+ 
+ 
     return 0;
 }
-
---
+++ horoscopo/src/TRIEbin.cpp
@@ -0,0 +1,207 @@
+#include "../include/bib.h"
+
+using namespace std;
+ 
+// Define o tamanho do caractere
+ 
+
+streampos root = -1; 
+
+
+Trie initT() {
+    Trie trie;
+    trie.isLeaf = false; 
+    for (int i = 0; i < CHAR_SIZE; i++) {
+            trie.character[i] = -1;
+    }
+    trie.pos = -1; 
+    return trie;
+}
+
+//funcao q coloca no arquivo um nodo da TRIE
+streampos putInArqT(Trie trie) {
+    int addressTrie;
+
+    ofstream triePut("trie.bin", ios_base::binary | ios_base::app);
+
+    if (!triePut) {                                   //arquivo nao foi criado/aberto
+        return -1;
+    }
+
+    triePut.write((char*)&trie, sizeof(Trie));
+
+    //cout << "colocando " << trie.pos << " no arquivo"; 
+
+    addressTrie = triePut.tellp();
+    addressTrie = addressTrie - sizeof(Trie);
+
+    //cout << " na posicao " << addressTrie << endl;  
+ 
+    triePut.close(); 
+    return addressTrie; 
+
+}
+
+//funcao q le do arquivo um nodo da TRIE
+Trie readInArqT(streampos pos) {
+    Trie trie; 
+    ifstream triePut("trie.bin", ios::binary);
+
+    //vou ate o endereco desse nodo e leio
+    triePut.seekg(pos); 
+    triePut.read((char*)&trie, sizeof(Trie)); 
+
+    triePut.close();
+    return trie; 
+}
+
+int swapStructT(streampos posi, Trie trie){
+    
+    Trie trieAux; 
+    trieAux = trie; 
+
+    //cout << "fazendo o swap de " << trie.pos;
+
+    ofstream trieSwap("trie.bin", ios::binary | ios::in);
+        
+    //move o ponteiro ate a posicao da string antiga 
+    trieSwap.seekp(posi); 
+    
+   // cout << endl << "colocando " << trie.pos << " no arquivo na posicao " << posi << endl; 
+
+
+    //escrevo nodo no arquivo nessa posicao
+    trieSwap.write((char*)&trieAux, sizeof(Trie));        
+    
+    trieSwap.close();
+    return 0; 
+}
+
+
+
+// Função iterativa para inserir uma chave em um Trie
+void insertT(string key, streampos pos) {
+    
+
+    Trie rootTrie; 
+    Trie auxTrie; 
+    Trie currentTrie; 
+
+    streampos auxPos; 
+    streampos currentPos; 
+
+    rootTrie = readInArqT(root); 
+
+    currentTrie = rootTrie; 
+    currentPos = root; 
+
+
+    for (int i = 0; i < key.length(); i++) {
+
+        if (currentTrie.character[key[i]] == -1) {
+
+            //trie auxiliar serve para criar novo nodo
+            //aux pos idem, sao so passageitos
+            auxTrie = initT(); 
+            auxPos = putInArqT(auxTrie); 
+            currentTrie.character[key[i]] = auxPos;
+
+            swapStructT(currentPos, currentTrie); 
+        }
+        currentPos = currentTrie.character[key[i]];
+        currentTrie = readInArqT(currentPos); 
+    }
+ 
+    // marca o nodo atual como uma folha
+    currentTrie.isLeaf = true; 
+    currentTrie.pos = pos; 
+
+    swapStructT(currentPos,currentTrie); 
+}
+
+// Função iterativa para pesquisar uma chave em um Trie. Ele retorna verdadeiro
+// se a chave for encontrada no Trie; caso contrário, retorna falso
+streampos searchT(string key) {
+    //nao sei o que fazer com isso aq
+
+    // retorna -1 se Trie estiver vazio
+    if (root == -1) {
+        return -1;
+    }
+ 
+    Trie rootTrie; 
+    Trie auxTrie; 
+    Trie currentTrie; 
+
+    streampos auxPos; 
+    streampos currentPos; 
+
+    rootTrie = readInArqT(root); 
+
+    currentTrie = rootTrie; 
+    currentPos = root; 
+
+
+    for (int i = 0; i < key.length(); i++) {
+        // vai para o próximo nó
+
+        currentPos = currentTrie.character[key[i]];
+        currentTrie = readInArqT(currentPos);
+        //cout << currentTrie.pos; 
+        
+         // se a string for inválida (alcançou o final de um caminho no Trie)
+        if (currentPos == -1) {
+            return -1;
+        }
+    }
+    // retorna true se o nó atual for uma folha e o
+    // fim da string é atingido
+    return currentPos;
+}
+  
+// Implementação C++ da estrutura de dados Trie
+int criaTrie() {
+    //root ja existe e esta definida la em cima
+
+    Trie auxPrint; 
+    streampos auxPos = -1; 
+
+    Trie rootTrie = initT(); 
+
+    root = putInArqT(rootTrie);
+
+    insert("hello",2);
+
+    auxPos = searchT("hello");
+    cout << "o "; 
+    //cout << auxPos; 
+    auxPrint = readInArqT(auxPos); 
+    cout << auxPrint.pos << " ";  
+
+ 
+    insert("helloworld",4);
+    auxPos = searchT("helloworld");
+    cout << "o "; 
+    //cout << auxPos; 
+    auxPrint = readInArqT(auxPos); 
+    cout << auxPrint.pos << " ";  
+ 
+ 
+    insert("hell",5);
+    auxPos = searchT("hell");
+    cout << "o "; 
+    //cout << auxPos;     
+    auxPrint = readInArqT(auxPos); 
+    cout << auxPrint.pos << " ";
+ 
+    insert("h",6);
+    auxPos = searchT("h");
+    cout << "o "; 
+    //cout << auxPos;     
+    auxPrint = readInArqT(auxPos); 
+    cout << auxPrint.pos << " "; 
+    cout << endl;
+ 
+ 
+    return 0;
+}
